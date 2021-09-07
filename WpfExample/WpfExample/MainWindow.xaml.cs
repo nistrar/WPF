d29e21def3f3c9025ad57bdd25c010a3a52f2953
Dan Costea <costea.dan.cristian@gmail.com>
2021-09-07 19:34:54 +0200

    Sum Example with Data binding and INotifyChanges

--- WpfExample/WpfExample/MainWindow.xaml.cs
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -20,76 +20,15 @@ namespace WpfExample
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int MyProperty {
-            get { return (int)GetValue(myDependencyProperty); }
-            set { SetValue(myDependencyProperty, value); } 
-        }
-
-        public static readonly DependencyProperty myDependencyProperty = 
-            DependencyProperty.Register("MyProperty", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+        public Sum SumObj { get; set; }
 
         public MainWindow()
         {
             InitializeComponent();
-            MySlider.Value = 30;
-            MyTextBox.Text = MySlider.Value.ToString();
 
-            /*
-            Grid grid = new Grid();
-            this.Content = grid;
-            Button btn = new Button();
-            btn.FontSize = 32;
-            btn.Width = 300;
-            btn.Height = 100;
-            WrapPanel wrapPanel = new WrapPanel();
-
-            TextBlock txtBlock = new TextBlock();
-            txtBlock.Text = "Multi";
-            txtBlock.Foreground = Brushes.Blue;
-            wrapPanel.Children.Add(txtBlock);
-
-            txtBlock = new TextBlock();
-            txtBlock.Text = "Color";
-            txtBlock.Foreground = Brushes.Red;
-            wrapPanel.Children.Add(txtBlock);
-
-            txtBlock = new TextBlock();
-            txtBlock.Text = "Button";
-            txtBlock.Foreground = Brushes.Yellow;
-            wrapPanel.Children.Add(txtBlock);
-
-            btn.Content = wrapPanel;
-
-            grid.Children.Add(btn);
-            */
-
-            
+            SumObj = new Sum { Num1 = "1", Num2 = "3" };
+            this.DataContext = SumObj;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Button clicked - Direct event");
-        }
-
-        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Button mouse up - Bubbling Event out of the tree");
-        }
-
-        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Preview mouse up - Tunneling Event into the tree");
-        }
-
-        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Preview MouseLeftButtonDown - Tunneling Event into the tree");
-
-        }
-
-        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            MessageBox.Show("Preview MouseRightButtonUp - Tunneling Event into the tree");
-        }
     }
 }
