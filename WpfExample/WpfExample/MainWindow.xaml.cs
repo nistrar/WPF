3dd2ea360baab133ca2b268f6adbf984bc79d0ca
Dan Costea <costea.dan.cristian@gmail.com>
2021-09-14 15:21:05 +0200

    ComboBox

--- WpfExample/WpfExample/MainWindow.xaml.cs
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -28,30 +28,11 @@ namespace WpfExample
             Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
 
             InitializeComponent();
-            
-            List<Match> matches = new List<Match>
-            {
-                new Match { Team1 = "FC Bayern", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 86 },
-                new Match { Team1 = "PSG", Team2 = "Juventus", Score1 = 2, Score2 = 2, Completion = 36 },
-                new Match { Team1 = "AC Milan", Team2 = "Barcelona", Score1 = 1, Score2 = 5, Completion = 78 },
-                new Match { Team1 = "Arsenal", Team2 = "Manchester City", Score1 = 1, Score2 = 1, Completion = 28 }
-            };
 
-            
 
-            lbMatches.Items.Clear();
-            lbMatches.ItemsSource = matches;
-
+            comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (lbMatches.SelectedItem !=null)
-            MessageBox.Show("Ausgewältes Spiel:" +
-                (lbMatches.SelectedItem as Match).Team1 + " " +
-                (lbMatches.SelectedItem as Match).Score1 + " - " +
-                (lbMatches.SelectedItem as Match).Score2 + " " +
-                (lbMatches.SelectedItem as Match).Team2);
-        }
+
     }
 }
