45cf87b411d0047de42775cde2be08abd94a498e
Dan Costea <costea.dan.cristian@gmail.com>
2021-09-14 15:14:19 +0200

    Fußball MAtches

--- WpfExample/WpfExample/MainWindow.xaml.cs
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,15 +22,36 @@ namespace WpfExample
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Sum SumObj { get; set; }
 
         public MainWindow()
         {
-            InitializeComponent();
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-us");
 
-            SumObj = new Sum { Num1 = "1", Num2 = "3" };
-            this.DataContext = SumObj;
+            InitializeComponent();
+            
+            List<Match> matches = new List<Match>
+            {
+                new Match { Team1 = "FC Bayern", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 86 },
+                new Match { Team1 = "PSG", Team2 = "Juventus", Score1 = 2, Score2 = 2, Completion = 36 },
+                new Match { Team1 = "AC Milan", Team2 = "Barcelona", Score1 = 1, Score2 = 5, Completion = 78 },
+                new Match { Team1 = "Arsenal", Team2 = "Manchester City", Score1 = 1, Score2 = 1, Completion = 28 }
+            };
+
+            
+
+            lbMatches.Items.Clear();
+            lbMatches.ItemsSource = matches;
+
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem !=null)
+            MessageBox.Show("Ausgewältes Spiel:" +
+                (lbMatches.SelectedItem as Match).Team1 + " " +
+                (lbMatches.SelectedItem as Match).Score1 + " - " +
+                (lbMatches.SelectedItem as Match).Score2 + " " +
+                (lbMatches.SelectedItem as Match).Team2);
+        }
     }
 }
