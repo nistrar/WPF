b08867411ab0764ca2faf6eadb72d79cd2c6aef5
Dan Costea <costea.dan.cristian@gmail.com>
2021-09-14 18:23:59 +0200

    XAML triggers

--- WpfExample/WpfExample/MainWindow.xaml.cs
+++ WpfExample/WpfExample/MainWindow.xaml.cs
@@ -29,36 +29,7 @@ namespace WpfExample
 
             InitializeComponent();
 
-
         }
 
-        private void cbAllToppings_Checked(object sender, RoutedEventArgs e)
-        {
-            bool newVal = (bool)cbAllToppings.IsChecked;
-            cbSalami.IsChecked = newVal;
-            cbMushrooms.IsChecked = newVal;
-            cbMozzarella.IsChecked = newVal;
-        }
-
-        private void cbAllToppings_Unchecked(object sender, RoutedEventArgs e)
-        {
-            bool newVal = (bool)cbAllToppings.IsChecked;
-            cbSalami.IsChecked = newVal;
-            cbMushrooms.IsChecked = newVal;
-            cbMozzarella.IsChecked = newVal;
-        }
-
-        private void cbSingleCheckChanged(object sender, RoutedEventArgs e)
-        {
-            cbAllToppings.IsChecked = null;
-            if ((cbSalami.IsChecked == true) && (cbMushrooms.IsChecked == true) && (cbMozzarella.IsChecked == true))
-            {
-                cbAllToppings.IsChecked = true;
-            }
-            else if ((cbSalami.IsChecked == false) && (cbMushrooms.IsChecked == false) && (cbMozzarella.IsChecked == false))
-            {
-                cbAllToppings.IsChecked = false;
-            }
-        }
     }
 }
